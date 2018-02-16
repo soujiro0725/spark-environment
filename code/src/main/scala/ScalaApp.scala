07dc2efa57290008f72e19eca7051545fabf32db
soujiro0725 <soujiro0725@gmail.com>
2018-02-16 21:27:09 +0900

    classifier modified

--- code/src/main/scala/ScalaApp.scala
+++ code/src/main/scala/ScalaApp.scala
@@ -1,6 +1,8 @@
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 /**
   * a simple spark app in Scala
@@ -12,12 +14,29 @@ object ScalaApp {
       .setAppName("simple app")
       .setMaster("local")
     val sc = new SparkContext(configuration)
-    val logFile = "/Users/soichi/Projects/spark-environment/README.md"
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a $numAs, Lines with b: $numBs")
-    spark.stop()
+
+    val spark = SparkSession.builder().getOrCreate()
+
+    val data = spark.read.format("libsvm")
+      .load("/Users/soichi/Projects/spark-environment/code/data/sample_multiclass_classification_data.txt")
+
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
+    val layers = Array[Int](4, 5, 4, 3)
+
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(layers)
+      .setBlockSize(128)
+      .setSeed(1234L)
+      .setMaxIter(100)
+
+    val model = trainer.fit(train)
+    val result = model.transform(test)
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+
+    println("test set accuracy = " + evaluator.evaluate(predictionAndLabels))
   }
 }
