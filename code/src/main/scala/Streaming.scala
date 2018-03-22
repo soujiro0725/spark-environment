7afc9f5cd84d820501d42cf0d372f034ab269bb5
sishida <sishida@gsx.co.jp>
2018-03-23 08:28:07 +0900

    streaming modified

--- code/src/main/scala/Streaming.scala
+++ code/src/main/scala/Streaming.scala
@@ -1,5 +1,7 @@
 import java.io.PrintWriter
 import java.net.ServerSocket
+import java.text.{SimpleDateFormat, DateFormat}
+import java.util.Date
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -67,6 +69,42 @@ object SimpleStreamingApp {
     val stream  =ssc.socketTextStream("localhost", 9999)
 
     stream.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
+
+
+object StreamingAnalyticsApp {
+  def main(args: Array[String]) {
+    val ssc = new StreamingContext("local[2]", "first streaming app", Seconds(10))
+    val stream = ssc.socketTextStream("localhost", 9999)
+
+    val events = stream.map { record =>
+      val event = record.split(",")
+      (event(0), event(1), event(2))
+    }
+
+    events.foreachRDD { (rdd, time) =>
+      val numPurchases = rdd.count()
+      val uniqueUsers = rdd.map { case (user, _, _) => user }.distinct().count()
+      val totalRevenue = rdd.map { case (_, _, price) => price.toDouble }.sum()
+      val productByPopularity = rdd
+        .map { case (user, product, price) => (product, 1) }
+        .reduceByKey(_ + _)
+        .collect()
+        .sortBy(-_._2)
+      val mostPopular = productByPopularity(0)
+
+      val formatter = new SimpleDateFormat
+      val dateStr = formatter.format(new Date(time.milliseconds))
+      println(s"== Batch start time: $dateStr ==")
+      println("total purchases: " + numPurchases)
+      println("unique users: " + numPurchases)
+      println("total revenues: " + uniqueUsers)
+      println("most popular product: %s with %d purchases".format(mostPopular._1, mostPopular._2))
+    }
+
     ssc.start()
     ssc.awaitTermination()
   }
