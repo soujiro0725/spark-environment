9ba11ac500a0f40bd3ff986f073cef88086fcbff
sishida <sishida@gsx.co.jp>
2018-03-26 12:52:16 +0900

    streaming regression added

--- code/src/main/scala/Streaming.scala
+++ code/src/main/scala/Streaming.scala
@@ -109,3 +109,32 @@ object StreamingAnalyticsApp {
     ssc.awaitTermination()
   }
 }
+
+object StreamingStateApp {
+  import org.apache.spark.streaming.StreamingContext._
+
+  def updateState(prices: Seq[(String, Double)], currentTotal: Option[(Int, Double)]) = {
+    val currentRevenue = prices.map(_._2).sum
+    val currentNumberPurchases = prices.size
+    val state = currentTotal.getOrElse((0, 0.0))
+    Some((currentNumberPurchases + state._1, currentRevenue + state._2))
+  }
+
+  def main(args: Array[String]) {
+    val ssc = new StreamingContext("local[2]", "First streaming app", Seconds(10))
+    ssc.checkpoint("/tmp/socketTextStreaming")
+    val stream = ssc.socketTextStream("localhost", 9999)
+
+    val events = stream.map { record =>
+      val event = record.split(",")
+      (event(0), event(1), event(2).toDouble)
+    }
+
+    val users = events.map{ case (user, product, price) => (user, (product, price)) }
+    val revenuePerUser = users.updateStateByKey(updateState)
+    revenuePerUser.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
